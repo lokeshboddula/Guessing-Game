ff5ee3dd2e0e65a632b05e7fee00d189a4ecde9d
lokesh boddula <lokesh.immanuel555@gmail.com>
2020-09-21 10:41:20 +0530

    minimal changes ul etc

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
  var randomNumber = Math.floor(Math.random() * 100 + 1);
-  document.getElementById("start-button").addEventListener("click",startAction);
+    document.getElementById("start-button").addEventListener("click",startAction);
     function startAction() {
-    var btn = document.getElementById("start-button");
-    btn.remove();
-    document.getElementById('hi').innerHTML = 'Hi, Am a Number!, guess my Value!!';
-    document.getElementById('hint').innerHTML = 'Hint : my value is in between 1 to 100!';
-    document.getElementById('below').innerHTML = 'Start guessing below!';
-
-   var input = document.getElementById('inputNumber');
-    input.style.display = "block";
-
-    var btnVisibility = document.getElementById('check-button');
-    btnVisibility.style.visibility = "visible";
+    document.getElementById('start-button').style.display = 'none';
+    document.getElementById("game").style.display = 'block';
+    
    };
+
    var count = 10;
    document.getElementById("check-button").addEventListener("click",checkAction);
    function  checkAction() {
@@ -20,18 +13,27 @@
    
    if( guessedNumber > randomNumber && count > 1) {
      count= count - 1 ;
-     document.getElementById("result").innerHTML = "my value is bit low!!, you have " + count + " more chances to guess!"; 
+     var node = document.createElement("li");
+     node.className = 'list-group-item';
+     var textnode = document.createTextNode(`your guess : ${guessedNumber} is bit high, try again!`);
+     node.appendChild(textnode);
+     document.getElementById("myList").appendChild(node);
    }
    else if (guessedNumber < randomNumber  && count > 1) {
     count = count - 1;
-     document.getElementById("result").innerHTML = "my value is bit higher!! , you have " +count + " more chances to guess!"; 
+    var node = document.createElement("li");
+    node.className = 'list-group-item mr-4';
+    var textnode = document.createTextNode(`your guess :${guessedNumber} is bit less, try again!`);
+    node.appendChild(textnode);
+    document.getElementById("myList").appendChild(node);
    }
-   else if (guessedNumber == randomNumber && count > 1) {
-    document.getElementById("result").innerHTML = " you guessed it right!, I'm " + randomNumber +
-     "!!\n press F5 key or refresh the page to play again!";
+
+   else if (guessedNumber == randomNumber && count > 0) {
+    document.getElementById("result").innerHTML = ` you guessed it right!, I'm ${randomNumber}!!
+    you guessed me in ${11 - count} guesses!`;
    }
+
    else {
-    document.getElementById("result").innerHTML = " You failed to guess!, I'm " + randomNumber +
-     "!!\n press F5 key or refresh the page to play again!";
-   }
+    document.getElementById("result").innerHTML = " You failed to guess!, I'm " + randomNumber + " !!";
+    }
    };
