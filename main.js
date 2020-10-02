41b7984071ed376cfa8a2b45edbbc52f732938fc
lokesh boddula <lokesh.immanuel555@gmail.com>
2020-10-03 01:24:55 +0530

    react reverted and game activated

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
-/* nothing here  */
+ var randomNumber = Math.floor(Math.random() * 100 + 1);
+    document.getElementById("start-button").addEventListener("click",startAction);
+    function startAction() {
+    document.getElementById('start-button').style.display = 'none';
+    document.getElementById("game").style.display = 'block';
+    
+   };
+
+   var count = 10;
+   document.getElementById('span').innerHTML = count;
+   document.getElementById("check-button").addEventListener("click",checkAction);
+   function  checkAction() {
+   var guessedNumber = document.getElementById("inputNumber").value;
+   document.getElementById('span').innerHTML = count;
+   if( guessedNumber > randomNumber && count > 1) {
+     count= count - 1 ;
+     var node = document.createElement("li");
+     node.className = 'list-group-item';
+     var textnode = document.createTextNode(`your guess : ${guessedNumber} is bit high, try again!`);
+     node.appendChild(textnode);
+     document.getElementById("myList").appendChild(node);
+     document.getElementById('span').innerHTML = Math.min(Math.max(parseInt(count), 0), 10);
+   }
+   else if (guessedNumber < randomNumber  && count > 1) {
+    count = count - 1;
+    var node = document.createElement("li");
+    node.className = 'list-group-item';
+    var textnode = document.createTextNode(`your guess : ${guessedNumber} is bit less, try again!`);
+    node.appendChild(textnode);
+    document.getElementById("myList").appendChild(node);
+    document.getElementById('span').innerHTML = Math.min(Math.max(parseInt(count), 0), 10);
+
+   }
+
+   else if (guessedNumber == randomNumber && count > 0) {
+    count = count - 1;
+    document.getElementById("result").innerHTML = ` you guessed it right!, I'm ${randomNumber}!!
+    you guessed me in ${10 - count} guesses!`;
+    document.getElementById('span').innerHTML = Math.min(Math.max(parseInt(count), 0), 10);
+   }
+
+   else {
+    document.getElementById("result").innerHTML = " You failed to guess!, I'm " + randomNumber + " !!";
+    count = count - 1;
+    document.getElementById('span').innerHTML = Math.min(Math.max(parseInt(count), 0), 10);
+
+    }
+   };
